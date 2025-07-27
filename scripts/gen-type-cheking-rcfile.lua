8bb81e18ac22f0fe5cbc94dd1115ba7918e51283
Caetano Genete <caetanogenete@gmail.com>
2025-07-27 22:18:10 +0100

    hopefully fixed typing issues

--- scripts/gen-type-cheking-rcfile.lua
+++ scripts/gen-type-cheking-rcfile.lua
@@ -1,6 +1,9 @@
 -- Use the NeoVim runtime to generate a LuaLS rc file.
 
 local luarc = {
+	runtime = {
+		version = "Lua 5.4",
+	},
 	workspace = {
 		library = vim.api.nvim_get_runtime_file("", true),
 	},
