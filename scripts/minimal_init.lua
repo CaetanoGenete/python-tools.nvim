4bf6bf96d7f0a7fc2adcd0fb52b2a442d99f21a7
Caetano Genete <caetanogenete@gmail.com>
2025-11-06 23:29:33 +0000

    Now properly linking file

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -2,12 +2,10 @@ local py_parser_path
 
 if vim.fn.has("win32") == 1 then
 	vim.g.pytools_default_python_path = "./test/fixtures/mock-repo/.venv/Scripts/python.exe"
-	-- py_parser_path = "./tree-sitter-python/python.dll"
-	-- py_parser_path = "./misc/python.so"
 	py_parser_path = "./build-ts/install/bin/tree-sitter-python.dll"
 else
 	vim.g.pytools_default_python_path = "./test/fixtures/mock-repo/.venv/bin/python"
-	py_parser_path = "./build-ts/install/bin/tree-sitter-python.so"
+	py_parser_path = "./build-ts/install/lib/libtree-sitter-python.so"
 end
 
 vim.treesitter.language.add("python", { path = py_parser_path })
