90e1f0d0c3cdd42ab90a463e4181ee60589354e9
Caetano Genete <caetanogenete@gmail.com>
2025-11-12 23:26:49 +0000

    Hopefully fixed CI issue

--- lua/_meta/test/asserts.lua
+++ lua/_meta/test/asserts.lua
@@ -1,25 +1,23 @@
 ---@meta
 
----@class luassert
-local luassert = {}
+---@class luassert.internal
+local internal = {}
 
 --- Polls for until `fn` returns true. If `opts.timeout` is reached before `fn` returns true, the
 --- assertion fails.
 ---
 ---@param fn fun():boolean The polling function.
 ---@param opts? AssertPollOpts Additional options.
-function luassert.poll(fn, opts) end
+function internal.poll(fn, opts) end
 
 --- Same as `assert.same`, but only compares on the keys available in `lhs`.
 ---
 ---@param lhs table<string, any>
 ---@param rhs table<string, any>
-function luassert.subset(lhs, rhs) end
+function internal.subset(lhs, rhs) end
 
 --- Compares paths after normalisation.
 ---
 ---@param expected string
 ---@param actual string
-function luassert.paths_same(expected, actual) end
-
-return luassert
+function internal.paths_same(expected, actual) end
