156e09e823f2f4343b860339c8cb9e69d5cf70d2
Caetano Genete <caetanogenete@gmail.com>
2026-02-28 14:02:15 +0000

    Now building from within lua

--- lua/python_tools/init.lua
+++ lua/python_tools/init.lua
@@ -2,8 +2,6 @@ local M = {}
 
 local async = require("python_tools._async")
 
-local ROOT = vim.fs.normalize(vim.fs.abspath(debug.getinfo(1).source:match("@?(.*/)") .. "../../"))
-
 ---@param cmd string
 ---@param exts string[]
 ---@return string?
@@ -24,55 +22,116 @@ local function executable(cmd, exts)
 	return nil
 end
 
-local BUILD_PATH = "build-ts"
+local BUILD_PATH = "build"
+local INSTALL_PATH = "lib"
 
-local function build_or_install()
-	-- Try build from source
-
+---@async
+---@param cwd string
+local function build_or_install(cwd)
 	local cmake = executable("cmake", { ".exe" })
 	if cmake then
-		local result = async.system({
-			cmake,
-			"-S",
-			".",
-			"-B",
-			BUILD_PATH,
-			"-DCMAKE_BUILD_TYPE=release",
-			"-DCMAKE_EXPORT_COMPILE_COMMANDS=ON",
-			"-DBUILD_REDIRECTLIB=ON",
-		}, { cwd = ROOT })
+		vim.notify("cmake found! Trying to build using cmake.", vim.log.levels.INFO)
 
-		if result.code == 0 then
+		local cmds = {
+			{
+				cmake,
+				"-S",
+				".",
+				"-B",
+				BUILD_PATH,
+				"-DCMAKE_BUILD_TYPE=release",
+				"-DCMAKE_EXPORT_COMPILE_COMMANDS=ON",
+				"-DBUILD_REDIRECTLIB=ON",
+			},
+			{
+				cmake,
+				"--build",
+				BUILD_PATH,
+				"--config",
+				"release",
+			},
+			{
+				cmake,
+				"--install",
+				BUILD_PATH,
+				"--prefix",
+				INSTALL_PATH,
+			},
+		}
+
+		local ok = true
+		for step, cmd in ipairs(cmds) do
+			local result = async.system(cmd, { cwd = cwd })
+			if result.code ~= 0 then
+				ok = false
+
+				vim.notify(
+					("cmake step %d failed with exit code: %d"):format(step, result.code),
+					vim.log.levels.INFO
+				)
+				vim.notify(result.stderr, vim.log.levels.ERROR)
+				break
+			end
+		end
+
+		if ok then
+			vim.notify("cmake install succeeded!")
 			return
 		end
 	end
 
 	local luarocks = executable("luarocks", { ".exe", ".bat" })
 	if luarocks then
+		vim.notify("luarocks found! Trying to build using `luarocks make`.", vim.log.levels.INFO)
+
 		local result = async.system({
-			cmake,
-			"-S",
-			".",
-			"-B",
-			BUILD_PATH,
-			"-DCMAKE_BUILD_TYPE=release",
-			"-DCMAKE_EXPORT_COMPILE_COMMANDS=ON",
-			"-DBUILD_REDIRECTLIB=ON",
-		}, { cwd = ROOT })
+			luarocks,
+			"make",
+			"--no-install",
+		}, { cwd = cwd })
 
 		if result.code == 0 then
-			return
+			local libpath = vim.fs.joinpath(cwd, "python_tools")
+			-- Note: `luarocks make` install clib relative to the current directory (There is no way to
+			-- change this at the moment...), move the binaries to the lib/.
+			local ok = os.rename(libpath, vim.fs.joinpath(cwd, "lib/python_tools"))
+			if ok then
+				vim.notify("Luarocks install succeeded!")
+				return
+			end
+
+			vim.notify(
+				"luarocks make succeeded, but failed to install. cleaning up...",
+				vim.log.levels.INFO
+			)
+
+			vim.fs.rm(libpath, { recursive = true, force = true })
+		else
+			vim.notify(
+				("`luarocks make` failed with exit code: %d"):format(result.code),
+				vim.log.levels.INFO
+			)
 		end
 	end
+
+	vim.notify(
+		"Failed to install python_tools clib! You may not be able to use certain features",
+		vim.log.levels.ERROR
+	)
+
+	-- TODO: Add download step if all else fails.
 end
 
 function M.setup()
+	local root =
+		vim.fs.normalize(vim.fs.abspath(debug.getinfo(1).source:match("@?(.*/)") .. "../../"))
+
 	-- Setup package path to be able to find c libraries
 	package.cpath = package.cpath
 		.. ";"
-		.. vim.fs.joinpath(ROOT, "lib/?.dll")
+		.. vim.fs.joinpath(root, INSTALL_PATH, "?.dll")
 		.. ";"
-		.. vim.fs.joinpath(ROOT, "lib/?.so")
+		.. vim.fs.joinpath(root, INSTALL_PATH, "?.so")
 		.. ";"
 
 	local ok = pcall(require, "python_tools.meta._pyproject")
@@ -80,7 +139,8 @@ function M.setup()
 		return
 	end
 
-	async.run(build_or_install)
+	vim.notify("clib not installed, trying to install now.", vim.log.levels.INFO)
+	async.run(build_or_install, root)
 end
 
 return M
